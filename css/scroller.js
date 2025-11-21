4c176c4f9e3453798b2a7f420cc57ebf2f2e0cbd
Cossete-github <165908488+Cossete-github@users.noreply.github.com>
2025-11-22 00:17:35 +0900

    Delete css directory

--- css/scroller.js
+++
@@ -1,9 +0,0 @@
-function scr(elementSelector) {
-    const e = document.querySelector(elementSelector);
-    const targetDOMRect = e.getBoundingClientRect();
-    const targetTop = targetDOMRect.top + window.pageYOffset + 56;
-    setTimeout(() => document.querySelector(elementSelector).scrollIntoView({
-        block: 'start',
-        behavior: 'smooth'
-    }), 166);
-}
